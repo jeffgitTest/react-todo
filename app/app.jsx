81a74fb47e0799df1e503bfa5087bfe2231f2806
Fluxion Inc <Fluxion Inc>
2016-10-12 15:27:26 +0800

    initial commit

---
+++ app/app.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+
+// Load foundation
+$(document).foundation();
+
+//app scss
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+  <p>BoilerPlate 3</p>,
+  document.getElementById('app')
+);
