312bc9cb0e436619ecee0bc9e50bb3ac428844de
Fluxion Inc <Fluxion Inc>
2016-10-12 17:41:56 +0800

    Add static todo list

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,9 +1,37 @@
 var React = require("react");
 
+var TodoList = require('TodoList');
+
 var TodoApp = React.createClass({
+  getInitialState: function () {
+    return{
+      todos: [
+        {
+          id: 1,
+          text: 'Feed the dog'
+        },
+        {
+          id: 2,
+          text: 'Study Programming'
+        },
+        {
+          id: 3,
+          text: 'Drive back home'
+        },
+        {
+          id: 4,
+          text: 'Sleep before 9pm'
+        }
+      ]
+    };
+  },
   render: function () {
+    var {todos} = this.state;
     return(
-      <div>Hello</div>
+      <div>Hello
+
+        <TodoList todos={todos}/>
+      </div>
     )
   }
 });
